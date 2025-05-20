d58dc70dd28d376f4c6481accf371af232d01490
Silas Bartol <silasbartol@gmail.com>
2025-05-20 14:23:48 -0400

    added functions to fetch remote and stored data

---
+++ js/data_fetch.js
@@ -0,0 +1,34 @@
+// URL for main config file data
+const configFileURL = 'https://script.google.com/macros/s/AKfycbzmPlGpgvvgZRqerq2xx-M_PikKISc6cLKy6Apkk1Rhefo7H12gJ9oVH8QlU0v09FiCDA/exec';
+
+// Runtime Variables
+let remoteFetchInProgress = false;
+
+
+/**
+ * Retrieves stored data from the browser's localStorage for the "mchs_schedule" key.
+ * If the data exists, it parses the JSON string into a JavaScript object.
+ * If the data does not exist, it returns null.
+ *
+ * @function
+ * @returns {Object|null} The parsed data object if it exists, or null if no data is stored.
+ */
+function storedData() {
+    let storedData = localStorage.mchs_schedule ? 
+        JSON.parse(localStorage.mchs_schedule): 
+        null;
+    return storedData
+}
+
+
+/**
+ * Fetches and returns JSON data from a remote URL specified by `configFileURL`.
+ *
+ * @async
+ * @function
+ * @returns {Promise<Object>} A promise that resolves to the parsed JSON data from the remote source.
+ */
+async function remoteData() {
+    let remoteData = await fetch(configFileURL).then(res=>res.json())
+    return remoteData
+}
